0c2aa448dc9c46ef5ecc6222bae3ef4c346c3f11
Felipe Manhani <fbmanhani@gmail.com>
2017-11-30 17:14:49 -0200

    Final adjusts using okta lib

--- src/main/java/com/manhani/spaoauthapp/MessageController.java
+++ src/main/java/com/manhani/spaoauthapp/MessageController.java
@@ -2,6 +2,7 @@ package com.manhani.spaoauthapp;
 
 import java.security.Principal;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     @GetMapping("/mod")
+    @PreAuthorize("hasAuthority('Everyone') || #oauth2.hasScope('email')")
     public String getMessageOfTheDay(final Principal principal) {
         return "The message of the day is boring for user: " + principal.getName();
     }
